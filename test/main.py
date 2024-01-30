07f8166bf13a96f41a01b83ce41dd07426d4e837
Ryo Sajima <roy_chicago2007@yahoo.co.jp>
2024-01-30 16:53:11 +0900

    Add files via upload

--- test/main.py
+++ test/main.py
@@ -1,2 +1,15 @@
+import asyncio
+import random
+
+import js
+
+
 async def btn_click(event) -> None:
     print("btn_click")
+    secs = random.randint(1, 10)
+    await asyncio.sleep(secs)
+
+    if bool(random.getrandbits(1)):
+        js.btn_success()
+    else:
+        js.btn_failure()
