0304f2e72b55be63093c5fbf013a7fc8386f32da
Ryo-Sajima <roy_chicago2007@yahoo.co.jp>
2024-02-03 13:45:56 +0900

    comment

--- test/main.py
+++ test/main.py
@@ -2,16 +2,25 @@ import random
 import btncon
 
 import js
+import pyscript
+
+btn = btncon.ButtonController()
 
 def btn_click(event) -> None:
+    pyscript.display("Sending Trigger")
 
-    btn = btncon.ButtonController()
     btn.send_trigger()
+
+    pyscript.display("Sent Trigger")
 
     secs = random.randint(1, 10)
     btn.sleep(secs)
+
+    pyscript.display(f"Waited {secs} seconds")
 
     if bool(random.getrandbits(1)):
         js.btn_success()
     else:
         js.btn_failure()
+
+    pyscript.display("Changed button")
